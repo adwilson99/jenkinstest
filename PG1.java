f81ccc053e090eb348b0a5b6dd029223b4490df7
adwilson99 <adwilson99@gmail.com>
2021-06-17 11:19:22 +0800

    Removed testing library
    Testing library not required, selenium library contains all components needed

--- PG1.java
+++ PG1.java
@@ -11,7 +11,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.testng.annotations.Test;
 
 public class PG1 {
         
