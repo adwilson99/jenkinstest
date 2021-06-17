38f945480ca15e0fc059ad4ad0517d97f8184a4f
adwilson99 <adwilson99@gmail.com>
2021-06-17 11:16:43 +0800

    Changed to use firefox driver
    unable to get chrome working, trying firefox instead.

--- PG1.java
+++ PG1.java
@@ -1,24 +1,40 @@
 package newproject;
 //Automated Functional test case scripts in Selenium - Running in headless mode as no display on linux box.
+// Modififed for firefox as can't get chrome working
 
 import java.io.IOException;
-import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.Test;
 
 public class PG1 {
         
     public static void main(String[] args) {
 
-      ChromeOptions options = new ChromeOptions();
+//      ChromeOptions options = new ChromeOptions();
         
-      options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-      options.addArguments("--no-sandbox"); // Bypass OS security model
+//      options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+//      options.addArguments("--no-sandbox"); // Bypass OS security model
       
       // declaration and instantiation of objects/variables
-      System.setProperty("webdriver.gecko.driver","chromedriver");
-      WebDriver driver = new ChromeDriver(options);
+//      System.setProperty("webdriver.gecko.driver","chromedriver");
+ FirefoxBinary firefoxBinary = new FirefoxBinary();
+ firefoxBinary.addCommandLineOptions("--headless");
+ firefoxBinary.addCommandLineOptions("--no-sandbox");
+ System.setProperty("webdriver.gecko.driver", "/usr/bin/geckodriver");
+ FirefoxOptions firefoxOptions = new FirefoxOptions();
+ firefoxOptions.setBinary(firefoxBinary);
+ FirefoxDriver driver = new FirefoxDriver(firefoxOptions);
+            
+ //           WebDriver driver = new ChromeDriver(options);
+            
+            
       
       // Test Case Vairables
        String baseUrl = "http://34.87.187.29:8000/hello.html";
@@ -37,7 +53,7 @@ public class PG1 {
             System.out.println("Test Failed");
       }
     
-      // close chrome
+      // close chrome / firefox browser
       driver.close();
   }
 }
