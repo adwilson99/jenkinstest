802a924a5089373baeb4d5f7e1baa53814937d27
adwilson99 <adwilson99@gmail.com>
2021-06-17 14:03:32 +0800

    Update PG1.java

--- PG1.java
+++ PG1.java
@@ -5,7 +5,7 @@ package newproject;
 import java.io.IOException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
-import org.openqa.selenium.support;
+import org.openqa.selenium.support.ui.WebDriverWait;
 //import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
