0baa8ac2d57f9d7f6b61c7ada3fa07d9457792b5
adwilson99 <adwilson99@gmail.com>
2021-06-17 15:34:04 +0800

    Update PG1.java

--- PG1.java
+++ PG1.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.Keys;
 //import org.openqa.selenium.chrome.ChromeOptions;
 //import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.WebElement;
